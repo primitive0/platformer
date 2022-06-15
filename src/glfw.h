3be9aa4c25f2d602822230bd46575f7900fed7af
primitive0 <94762195+primitive0@users.noreply.github.com>
2022-06-16 02:16:26 +0300

    Disable including opengl headers

--- src/glfw.h
+++ src/glfw.h
@@ -1,10 +1,8 @@
 #pragma once
 
+#define GLFW_INCLUDE_NONE // to disable including opengl headers
 #define GLFW_INCLUDE_VULKAN
-
 #include <GLFW/glfw3.h>
-
-#undef GLFW_INCLUDE_VULKAN
 
 extern bool glfwInitialized;
 
